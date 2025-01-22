717bf13fa19ea077b3b6372c05554422524b7960
LIKHITH HN <2023aa05332@wilp.bits-pilani.ac.in>
2025-01-22 09:46:20 +0530

    First Commit
    updating database and creating the files

---
+++ main.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+import numpy as np
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from pathlib import Path
